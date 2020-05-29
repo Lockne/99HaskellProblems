8c66cb08117c36da77b863247a47e5974f672312
Lockne <varun.nk98@gmail.com>
2020-05-29 06:24:56 +0530

    added foldr solution to P05

--- Set1/Problem_05.hs
+++ Set1/Problem_05.hs
@@ -1,6 +1,10 @@
 module Problem_05
   ( myReverse
+  , myReverse'
   ) where
 
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
+
+myReverse' :: [a] -> [a]
+myReverse' = foldr (\x xs -> xs ++ [x]) []              
