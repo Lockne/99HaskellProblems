bc923e31d4cb14c5382f1d5cc847a9710154e8c3
Lockne <varun.nk98@gmail.com>
2020-06-18 09:30:46 +0530

    modified problem 23

--- Set3/Problem_23.hs
+++ Set3/Problem_23.hs
@@ -5,11 +5,10 @@ import System.Random
 -- take a list and extract a random element from it
 chooseRandom :: String -> IO Char
 chooseRandom xs =
-    randomRIO (0, length xs - 1) >>=
-    return . (\x -> xs !! x)
+   (xs !!) <$> randomRIO (0, length xs - 1)
 
-rnd_select :: String -> Int -> IO ()
-rnd_select string n = do
+rndSelect :: String -> Int -> IO ()
+rndSelect string n = do
   let loop k acc
         | k > n = putStrLn $ reverse acc
       loop k acc = do
@@ -20,7 +19,4 @@ rnd_select string n = do
 main = do
   string <- getLine
   n <- fmap read getLine :: IO Int
-  let loop k acc
-        | k > n = putStrLn $ reverse acc
-      loop k acc = chooseRandom string >>= \x -> loop (k + 1) (x : acc)
-  loop 1 []
+  rndSelect string n
